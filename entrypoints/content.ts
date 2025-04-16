766b539e23b9bc91797f4fe61a9719825e44585e
Mervin <mengqing723@gmail.com>
2025-04-16 19:47:50 +0800

    feat: Add Home & Privacy Pages

--- entrypoints/content.ts
+++
@@ -1,9 +0,0 @@
-export default defineContentScript({
-  matches: ['*://*/*'],
-  main(ctx) {
-    /* console.log('Hello content.', document.title)
-    browser.runtime.onMessage.addListener((message) => {
-      console.log('message', message)
-    }) */
-  },
-})
